ea31f0fc111949c85162a21677edf8de74e7dfd3
Thales Maciel <thalesmsm12@gmail.com>
2022-09-24 16:33:22 -0300

    cria tabela posts_categories atraves da migration

--- src/migrations/20220924190028-create-posts_categories.js
+++ src/migrations/20220924190028-create-posts_categories.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+    });
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  },
 };
